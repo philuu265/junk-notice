1c930dfd81c72842459896539e159684c2de901f
philuu265 <mon.bimbim@gmail.com>
2016-05-20 22:57:41 +0700

    init project structure

---
+++ src/main/resources/lovediary/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lovediaryApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lovediaryApp
+ */
+angular.module('lovediaryApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ src/main/resources/lovediary/app/scripts/js/common-scripts.js
@@ -0,0 +1,23 @@
+var Script = function () {
+
+
+//    tool tips
+
+  $('.tooltips').tooltip();
+
+//    popovers
+
+  $('.popovers').popover();
+
+//    bxslider
+
+  $('.bxslider').show();
+  $('.bxslider').bxSlider({
+    minSlides: 4,
+    maxSlides: 4,
+    slideWidth: 276,
+    slideMargin: 20
+  });
+
+
+}();
