50a652184b6559d3bcf5cf1eb325da341498beda
khanhnguyenhong <khanh.nguyen.k1set@eiu.edu.vn>
2016-06-29 09:39:32 +0700

    change ngRoute -> ui-oute

--- src/main/webapp/lovediary/dist/scripts/app.js
+++ src/main/webapp/lovediary/dist/scripts/app.js
@@ -8,38 +8,99 @@
  *
  * Main module of the application.
  */
-angular
+var app = angular
   .module('lovediaryApp', [
+    'ui.router',
+    'oc.lazyLoad',
     'ngAnimate',
     'ngCookies',
     'ngResource',
-    'ngRoute',
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl',
-        controllerAs: 'login'
-      })
-      .when('/registration', {
-        templateUrl: 'views/registration.html',
-        controller: 'RegistrationCtrl',
-        controllerAs: 'registration'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+
+app.config(function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, $controllerProvider, $locationProvider) {
+    //$routeProvider
+    //  .when('/', {
+    //      templateUrl: 'views/main.html',
+    //      controller: 'MainCtrl',
+    //      controllerAs: 'main'
+    //  })
+    //  .when('/about', {
+    //      templateUrl: 'views/about.html',
+    //      controller: 'AboutCtrl',
+    //      controllerAs: 'about'
+    //  })
+    //  .when('/login', {
+    //      templateUrl: 'views/login.html',
+    //      controller: 'LoginCtrl',
+    //      controllerAs: 'login'
+    //  })
+    //  .when('/registration', {
+    //      templateUrl: 'views/registration.html',
+    //      controller: 'RegistrationCtrl',
+    //      controllerAs: 'registration'
+    //  })
+    //  .otherwise({
+    //      redirectTo: '/'
+    //  });
+
+    app.controller = $controllerProvider.register;
+
+    $urlRouterProvider.otherwise('404');
+
+    $stateProvider
+        .state('main', {
+            url: '/',
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl',
+            controllerAs: 'main',
+            title: 'Main Page'
+        })
+        .state('about', {
+            url: '/about',
+            templateUrl: 'views/about.html',
+            controller: 'AboutCtrl',
+            controllerAs: 'about',
+            title: 'About Us'
+        })
+        .state('login', {
+            url: '/login',
+            templateUrl: 'views/login.html',
+            controller: 'LoginCtrl',
+            controllerAs: 'login',
+            title: 'Login Page'
+        })
+        .state('registration', {
+            url: '/registration',
+            templateUrl: 'views/registration.html',
+            controller: 'RegistrationCtrl',
+            controllerAs: 'registration',
+            title: 'Registration Page'
+        })
+        .state('pageNotFound', {
+            url: '/404',
+            templateUrl: '404.html',
+            title: '404 - Not Found'
+        })
+
+    $locationProvider.html5Mode(true);
+
+    function lazyLoad() {
+        var args = [];
+        for (var i = 0, n = arguments.length; i < n; i++) {
+            args[i] = arguments[i];
+        }
+        return {
+            load: ['$ocLazyLoad', function ($ocLazyLoad) {
+                return $ocLazyLoad.load(args);
+            }]
+        }
+    };
+});
+
+app.run(function ($rootScope, $state, authenService, $window) {
+    $rootScope.$on('$stateChangeSuccess', function (evt, toState, toParams, fromState, fromParams) {
+        $window.document.title = toState.title;
+    });
+});
